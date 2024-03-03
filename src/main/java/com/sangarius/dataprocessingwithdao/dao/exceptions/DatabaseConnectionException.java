c92bd779c309fe9caedd8b4b8868cd890710d250
Sangariusss <c.boklakh.volodymyr@student.uzhnu.edu.ua>
2024-03-03 23:09:36 +0200

    Completed implementation of DatabaseDataProcessingWithValidation project

---
+++ src/main/java/com/sangarius/dataprocessingwithdao/dao/exceptions/DatabaseConnectionException.java
@@ -0,0 +1,11 @@
+package main.java.com.sangarius.dataprocessingwithdao.dao.exceptions;
+
+public class DatabaseConnectionException extends RuntimeException {
+    public DatabaseConnectionException(String message) {
+        super(message);
+    }
+
+    public DatabaseConnectionException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ src/main/java/com/sangarius/dataprocessingwithdao/dao/exceptions/ValidationException.java
@@ -0,0 +1,7 @@
+package main.java.com.sangarius.dataprocessingwithdao.dao.exceptions;
+
+public class ValidationException extends RuntimeException {
+    public ValidationException(String message) {
+        super(message);
+    }
+}
